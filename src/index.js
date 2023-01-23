2533d1f91380dcc52eca616f9f413579866a6df6
Mateen <zahidx297@yahoo.com>
2023-01-22 22:33:59 -0500

    add new task button and more styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './style.css'
 
+const addTaskButton = document.getElementById('add-task');
+addTaskButton.addEventListener('click', () => {
+    console.log('adding task...');
+});
+
 console.log('Hello World!');
+
+function Task() {
+
+}
